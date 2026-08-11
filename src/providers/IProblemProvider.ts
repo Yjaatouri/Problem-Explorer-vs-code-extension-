4d7d945ac44dd20a10dbaa4dcf8ef0aad5d8722d
yahya Jaatouri <yahyajaatouri2000@gmail.com>
2026-08-12 00:15:15 +0100

    chore(repo): remove obsolete v1 artifacts

--- src/providers/IProblemProvider.ts
+++
@@ -1,9 +0,0 @@
-import { Disposable, Uri } from 'vscode';
-
-export interface IProblemProvider extends Disposable {
-  start(): void;
-  stop(): void;
-  refresh(): void;
-  /** Optional: incremental scan of specific files */
-  refreshUris?(uris: readonly Uri[]): void;
-}
